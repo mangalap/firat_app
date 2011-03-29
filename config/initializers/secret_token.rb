9c1c326a8983c26a65aa3d8867f7ea342f29b6f0
Mangala Prabhu <mangala.prabhu@gmail.com>
2011-03-29 10:41:11 -0700

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FiratApp::Application.config.secret_token = '72dd9abb38fb894ffb39e286b959040d2f8da4e737f58818a2463b0cbc6bdc86afaa70644643880cdf7af5671fbbd5835b613f63d618b68cb59816e2d4d912b1'
